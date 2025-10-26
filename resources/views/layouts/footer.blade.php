937d69c752d6701762cdc81add359bc86daf383d
yisefmahdi <yisefmahdi@gmail.com>
2025-10-26 16:24:40 +0200

    Delete resources directory

--- resources/views/layouts/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<!-- Footer opened -->
-	<div class="main-footer ht-40">
-		<div class="container-fluid pd-t-0-f ht-100p">
-			<span>الحقوق محفوظه © {{ now()->year }} </span>
-		</div>
-	</div>
-<!-- Footer closed -->
