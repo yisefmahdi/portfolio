7e306f70060f1dddd7794be26043f56e5b7a85d8
yisefmahdi <yisefmahdi@gmail.com>
2025-10-26 16:24:29 +0200

    Delete public directory

--- public/asset/js/header.js
+++
@@ -1,9 +0,0 @@
-// closeSidebar
-closeSidebar();
-function openSidebar() {
-  document.getElementById("mySidebar").style.display = "block";
-}
-
-function closeSidebar() {
-  document.getElementById("mySidebar").style.display = "none";
-}
